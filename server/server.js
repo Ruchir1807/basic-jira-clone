794a12e464daae95cde06a4147f26a693e34a1f4
Rogue1807 <150613271+Ruchir1807@users.noreply.github.com>
2026-05-23 14:15:26 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,9 @@ const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://basic-jira-clone.vercel.app"
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
